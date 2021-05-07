0885ed3f168ee2ad6b63e700155806e0c03fa9f1
Sam von der Dunk <samvonderdunk@hotmail.com>
2021-05-07 10:40:08 +0200

    Ignore obj/*.d and *.o

--- obj/FossilRecord.d
+++
@@ -1,4 +0,0 @@
-FossilRecord.o FossilRecord.d : ../src/FossilRecord.cc ../include/FossilRecord.hh \
- ../include/Prokaryote.hh ../include/Header.hh ../include/dSFMT.h \
- ../include/Genome.hh ../include/Bead.hh ../include/Gene.hh \
- ../include/TFBS.hh ../include/House.hh
--- obj/House.d
+++
@@ -1,2 +0,0 @@
-House.o House.d : ../src/House.cc ../include/House.hh ../include/Bead.hh \
- ../include/Header.hh ../include/dSFMT.h ../include/Header.hh
